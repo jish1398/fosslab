88a5851f171558dc6b8b2c3bf14fcbf29e8ed66b
Jishnu Nair <jishnunair1398@gmail.com>
2018-02-27 22:10:13 +0530

    Add files via upload

---
+++ calculator.sh
@@ -0,0 +1,37 @@
+#!/bin/bash\
+clear
+sum=0
+i="y"
+echo "Enter the first no:"
+read n1
+echo "Enter the second no:"
+read n2
+while [ $i = "y" ]
+do
+echo "1.Addition"
+echo "2.Subtraction"
+echo "3.Multiplication"
+echo "4.Division"
+read ch
+case $ch in
+	1)sum=`expr $n1 + $n2`
+		echo "sum="$sum;;
+	2)sum=`expr $n1 - $n2`
+		echo "Sub="$sum;;
+	3)sum=`expr $n2 \* $n1`
+		echo "mul="$sum;;
+	4)sum=`expr $n2 / $n1`
+		echo "div="$sum;;
+	*)echo "Invalid Choice"
+	esac
+echo "Do you want to continue(y/n):"
+read i
+
+if [ $i != "y" ]
+then
+	exit
+fi
+	done
+
+
+
